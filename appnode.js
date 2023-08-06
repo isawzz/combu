de8c26e32bff214bc06dd2902622715ddebfe994
isawzz <iwgajam@gmail.com>
2023-08-06 14:54:13 -0700

    das

--- appnode.js
+++ appnode.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 // Middleware to parse incoming JSON data
 app.use(bodyParser.json());
@@ -9,14 +10,22 @@ app.use(bodyParser.json());
 app.post('/saveData', (req, res) => {
   // Process the data from the request (you can save it to a file, database, etc.)
   const data = req.body;
-  console.log('Received data:', data);
+  console.log('Received data:', typeof(data));
+
+  // fs.writeFile('output.txt', data, (err) => {
+  //   if (err) {
+  //     console.error('Error saving the file:', err);
+  //   } else {
+  //     console.log('Data saved successfully!');
+  //   }
+  // });
 
   // Respond with a success message
-  res.json({ message: 'Data saved successfully!' });
+  res.json({ data: data, message: 'Data saved successfully!' });
 });
 
 // Start the server
-const port = 3000; // You can use any available port
+const port = 4001; // You can use any available port
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
