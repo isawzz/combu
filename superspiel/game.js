a3e3b8d1aea0586999a74c0bf034d0af6a9374ca
isawzz <iwgajam@gmail.com>
2023-08-30 15:41:56 -0700

    das

--- superspiel/game.js
+++ superspiel/game.js
@@ -1,7 +1,26 @@
 onload=start;
 
 function start(){
-	test0();
+	test1();
+}
+function test1(){
+
+	//get player data from server
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 function test0(){
 	console.log('hello, again!')
